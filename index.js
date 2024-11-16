45c3d813273ca315ae24aecc95213d19dc5b0b87
Kuba Klin <poczta@kubaklin.pl>
2024-11-16 13:25:18 +0100

    feat: calculator init

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨')
+const operationButtons = document.querySelectorAll('.operation-button');
+const equalButton = document.querySelector('#button-equal');
+const clearButton  = document.querySelector('#button-clear');
+const calculatorScreen = document.querySelector('#screen');
+const errorMessage = document.querySelector('.error-message');
+
+operationButtons.forEach(function(operationButton) {
+  operationButton.addEventListener('click', function() {
+    let operationValue = operationButton.innerText;
+
+    if (calculatorScreen.value.length < 9) {
+      calculatorScreen.value += operationValue;
+    } else {
+      errorMessage.innerText = 'max 9 digits';
+    }
+  })
+})
+
+equalButton.addEventListener('click', function() {
+  const operationResult = calculatorScreen.value;
+  const lastCharacterFromResult = operationResult.slice(-1);
+
+  if (lastCharacterFromResult.match(/[+-/*0]/) || operationResult === '') {
+    errorMessage.innerText = 'wrong format';
+  } else {
+    calculatorScreen.value = eval(operationResult);
+    errorMessage.innerText = '';
+  }
+})
+
+clearButton.addEventListener('click', function() {
+  calculatorScreen.value = '';
+  errorMessage.innerText = '';
+})
